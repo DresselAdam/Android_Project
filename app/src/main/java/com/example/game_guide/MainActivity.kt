4771a751e8a9b4d20b972bc22a23e7d9441393a3
DresselAdam <amdressel8336@eagle.fgcu.edu>
2019-06-07 21:03:09 -0400

    Added BottomNavigationView to main activity. Created navigation resource menu with 5 items

--- app/src/main/java/com/example/game_guide/MainActivity.kt
+++ app/src/main/java/com/example/game_guide/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.game_guide
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
